fc3de6bc5d23ee57027624b9382b38b8c9e21d91
marvinrgb <marvinwolfart2@gmail.com>
2023-03-27 21:12:46 +0200

    serverrrr backend trennung

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 import http from 'http';
 import { Server } from 'socket.io';
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: '*' } });
 const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,42 +17,28 @@ app.use(express.json());
 
 
 class Game {
-  constructor (max_players, captain_id, power_ups, duration_till_last_circle) {
-    this.max_players = max_players;
-    this.players.push(captain_id);
-    this.power_ups = power_ups;
-    this.captain = captain_id;
-    this.duration_till_last_circle = duration_till_last_circle;
+  code;
+  constructor (player) {
+    this.players.push(player);
+    this.captain = player.player_id;
+    this.code = Math.floor(Math.random() * 10000);
   }
-  max_players;
+  max_players = 10;
   players = [];
   power_ups = [];
   seeker;
   captain;
   startTime;
-  duration_till_last_circle;
+
+  joinPlayer(player_id) {
+    if (this.players.find((p) => p.id == player_id)) return;
+    this.players.push({
+      'id' : player_id
+    })
+  }
 }
 
 const games = [];
-
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
-
-
-/*
-game setting in body -> duration till last circle, max players, which powerups...
-*/
-app.post('/game/create', (req, res) => {
-  console.log(req.body)
-  let game = new Game(req.body.max_players, req.body.power_ups, req.body.duration_till_last_circle)
-  res.send("test")
-})
-
-app.post('/game/join/:game_id', (req, res) => {
-  
-})
 
 function getNewPlayer(username, socket_id) {
   let id = Math.floor(Math.random() * 1000000000);
@@ -61,6 +47,13 @@ function getNewPlayer(username, socket_id) {
     "username" : username,
     "socket_id" : socket_id
   }
+}
+
+function getPlayerBySocket(socket_id) {
+  return players.find((p) => p.socket_id == socket_id);
+}
+function getGameByCode(code) {
+  return games.find((g) => g.code == code);
 }
 
 let players = [];
@@ -82,6 +75,27 @@ io.on('connection', (socket) => {
     socket.emit('sign-up-new-id', {'player_id' : p.player_id});
     console.log('[socket - signUp] ' + p.username);
   });
+  socket.on('create-game', (data) => {
+    const game = new Game(getPlayerBySocket(socket.id))
+    games.push(game);
+    console.log("[games - create] Created Game with Code " + game.code);
+    socket.emit('game-joined', (game));
+    socket.join(String(game.code));
+  });
+  socket.on('join-game', (data) => {
+    const player = getPlayerBySocket(socket.id)
+    getGameByCode(data.code).joinPlayer(player);
+    const game = getGameByCode(data.code);
+    console.log(`[games - join] ${player.username} joined Game ${data.code}`);
+    socket.emit('game-joined', (game));
+    io.to(game.code).emit('player-joined', ({
+      'username' : player.username
+    }))
+    socket.join(String(game.code));
+  })
+  socket.on('start-game', (data) => {
+
+  })
 })
 
 
